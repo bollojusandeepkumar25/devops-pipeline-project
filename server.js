9d6310f543c0fe0439fcfc63a0a5289da0e85242
Sandeep <sandeep@example.com>
2025-10-20 22:23:34 +0530

    Updated workflow to use GitHub secrets

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
   res.send('Hello from the Automated Pipeline!');
 });
-app.listen(3000);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
